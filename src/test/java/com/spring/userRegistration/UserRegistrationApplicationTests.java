116c9a133187c6151531269e892f4040685d787e
Amit Kumar <amit909563@gmail.com>
2025-03-05 20:12:03 +0530

    Added: User Registration and Login Functionality Implementated

---
+++ src/test/java/com/spring/userRegistration/UserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.userRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
