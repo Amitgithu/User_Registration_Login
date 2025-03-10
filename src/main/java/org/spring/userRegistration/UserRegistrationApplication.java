471abb9fb872e4ea2dd1017bcc81e145c8f622bd
Amit Kumar <amit909563@gmail.com>
2025-03-10 20:31:13 +0530

    Fixes: Some issue in the controller code

--- src/main/java/org/spring/userRegistration/UserRegistrationApplication.java
+++ src/main/java/org/spring/userRegistration/UserRegistrationApplication.java
@@ -2,11 +2,17 @@ package org.spring.userRegistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class UserRegistrationApplication {
 
     public static void main(String[] args) {
+
+        Dotenv.configure()
+                .systemProperties()
+                .load();
+
         SpringApplication.run(UserRegistrationApplication.class, args);
     }
 
